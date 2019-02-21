64cfcb6d86eb3a8ec514384259423c27bd538e58
Ragusaen <rtollu18@student.aau.dk>
2019-02-21 09:49:33 +0100

    Added unit testing

---
+++ compression/UnitTesting/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+using Compression;
+
+namespace UnitTesting {
+    [TestFixture]
+    public class Tests {
+        [Test]
+        public void Test1() {
+            Assert.True(true);
+        }
+    }
+}
