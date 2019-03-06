b47b266d1de79a4b6b7eabf9676773153597ac6a
KristianOdum <kani18@student.aau.dk>
2019-03-06 13:34:33 +0100

    Testfile xdxd

--- compression/UnitTesting/Tests.cs
+++ compression/UnitTesting/Tests.cs
@@ -3,18 +3,32 @@ using NUnit.Framework;
 
 using Compression;
 
-namespace UnitTesting {
+namespace UnitTesting{
     [TestFixture, Category("DataFile")]
     public class DataFileTest{
         [Test]
         public void FileLoadsCorrectly() {
             DataFile file = new DataFile();
-            byte[] expected = {65, 66, 67};
-            string path = "res/testfile1";
-            
+            byte[] expected = {97, 98, 99};
+            string path = TestContext.CurrentContext.TestDirectory + "../../../res/testfile1";
+
             file.LoadFromFile(path);
             byte[] actual = file.GetBytes(0, 3);
 
-            Assert.Equals(expected, actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void FileLoadsCProgram() {
+            DataFile file = new DataFile();
+            byte[] expected = {};
+            string path = TestContext.CurrentContext.TestDirectory + "../../../res/testfile2";
+
+            file.LoadFromFile(path);
+            byte[] actual = file.GetBytes(0, 100);
+
+            Assert.AreEqual(expected, actual);
         }
     }
+}
+            
