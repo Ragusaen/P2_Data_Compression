838c131cda1c53249a6a5296220548761081d5a4
KristianOdum <kani18@student.aau.dk>
2019-03-06 13:34:33 +0100

    Testfile xdxd

--- compression/Compression/DataFile.cs
+++ compression/Compression/DataFile.cs
@@ -1,9 +1,21 @@
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
 namespace Compression {
     public class DataFile{
         private byte[] ByteArray;
 
-        public byte[] GetBytes(uint start, uint length) {}
-        
-        public void LoadFromFile(string path) {}
+        public byte[] GetBytes(uint start, uint len) {
+            byte[] result = new byte[len];
+            
+            Array.Copy(ByteArray, start, result, 0, len);
+            
+            return result;
+        }
+
+        public void LoadFromFile(string path) {
+            ByteArray = File.ReadAllBytes(path);
+        }
     }
 }
