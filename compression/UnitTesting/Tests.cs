f4a3a7acab431b20ffee28f0dbc0f608f04eac9f
Ragusaen <rtollu18@student.aau.dk>
2019-03-06 13:52:12 +0100

    added some unit tests

--- compression/UnitTesting/Tests.cs
+++ compression/UnitTesting/Tests.cs
@@ -19,15 +19,37 @@ namespace UnitTesting{
         }
 
         [Test]
-        public void FileLoadsCProgram() {
+        public void FileLoadsBinProgram() {
             DataFile file = new DataFile();
-            byte[] expected = {};
-            string path = TestContext.CurrentContext.TestDirectory + "../../../res/testfile2";
+            byte[] expected = {127, 69, 76, 70, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 62, 0, 1, 0, 0, 0, 16, 6, 0, 0, 0, 0, 0, 0, 64, 0, 0, 0, 0, 0, 0, 0, 176, 25};
+            string path = TestContext.CurrentContext.TestDirectory + "../../../res/testbin1";
 
             file.LoadFromFile(path);
-            byte[] actual = file.GetBytes(0, 100);
+            byte[] actual = file.GetBytes(0, (uint)expected.Length);
 
             Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ThrowsOutOfBoundsExceptionWhenGetStartAtLargerThanSize() {
+            DataFile file = new DataFile();
+            string path = TestContext.CurrentContext.TestDirectory + "../../../res/testfile1";
+
+            file.LoadFromFile(path);
+            TestDelegate act = () => file.GetBytes(4, 1);
+
+            Assert.Throws<IndexOutOfRangeException>(act);
+        }
+
+        [Test]
+        public void ThrowsOutOfBoundsExceptionWhenLengthTooLarge() {
+            DataFile file = new DataFile();
+            string path = TestContext.CurrentContext.TestDirectory + "../../../res/testfile1";
+
+            file.LoadFromFile(path);
+            TestDelegate act = () => file.GetBytes(1, 3);
+
+            Assert.Throws<IndexOutOfRangeException>(act);
         }
     }
 }
