9d4b85f3315eccf360daa9c5f958fe9c71d35e47
KristianOdum <kani18@student.aau.dk>
2019-03-12 11:23:35 +0100

    Added unittests

--- compression/UnitTesting/Tests.cs
+++ compression/UnitTesting/Tests.cs
@@ -224,18 +224,55 @@ namespace UnitTesting{
             ArraySegment<byte> needleAsSegment= new ArraySegment<byte>(needle,0,needle.Length);
             
             uint actual = FindMatchingBytes.FindArraySegment(haystack, needleAsSegment);
-                        
+            
             Assert.AreEqual(expected, actual);
         }
         [Test]
         public void FindMatchingBytesFindFullLengthNeedle() {
             byte[] haystack = {97, 98, 99, 100, 101, 114, 101, 115, 117, 108, 116, 100, 105, 110, 102, 97, 114};
             byte[] needle = {114, 101, 115, 117, 108, 116};
-            Nullable<MatchPointer> expected = new MatchPointer(5, (uint) needle.Length);
+            Nullable<MatchPointer> expected = new MatchPointer(5, (uint) 6);
 
             Nullable<MatchPointer> actual = FindMatchingBytes.FindLongestMatch(haystack, needle);
             
             Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void FindMatchingBytesFindSemiLengthNeedle() {
+            byte[] haystack = {97, 98, 99, 100, 101, 114, 101, 115, 117, 107, 116, 100, 105, 110, 102, 97, 114};
+            byte[] needle = {114, 101, 115, 117, 108, 116};
+            Nullable<MatchPointer> expected = new MatchPointer(5, (uint) 4);
+
+            Nullable<MatchPointer> actual = FindMatchingBytes.FindLongestMatch(haystack, needle);
+            
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void FindMatchingBytesDoNotFindAnyMatchReturnsNull() {
+            byte[] haystack = {97, 98, 99, 100, 101, 114, 101, 115, 117, 108, 116, 100, 105, 110, 102, 97, 114};
+            byte[] needle = {86, 102, 59, 108};
+
+            Nullable<MatchPointer> actual = FindMatchingBytes.FindLongestMatch(haystack, needle);
+            
+            Assert.IsNull(actual);
+        }
+        [Test]
+        public void FindMatchingBytesDoNotFindAnyMatchEmptyHaystack() {
+            byte[] haystack = {};
+            byte[] needle = {86, 102, 59, 108};
+
+            Nullable<MatchPointer> actual = FindMatchingBytes.FindLongestMatch(haystack, needle);
+            
+            Assert.IsNull(actual);
+        }
+        [Test]
+        public void FindMatchingBytesDoNotFindAnyMatchEmptyNeedle() {
+            byte[] haystack = {97, 98, 99, 100, 101, 114, 101, 115, 117, 108, 116, 100, 105, 110, 102, 97, 114};
+            byte[] needle = {};
+
+            Nullable<MatchPointer> actual = FindMatchingBytes.FindLongestMatch(haystack, needle);
+            
+            Assert.IsNull(actual);
         }
     }
 }
