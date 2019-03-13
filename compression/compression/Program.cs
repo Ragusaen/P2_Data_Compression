a5dcbf56ad418329009ecd26612a10557bf6dc86
Ragusaen <rtollu18@student.aau.dk>
2019-03-13 11:14:36 +0100

    Added more files

--- compression/compression/Program.cs
+++ compression/compression/Program.cs
@@ -5,7 +5,7 @@ namespace compression {
     internal class Program {
         public static void Main(string[] args) {
 
-            string path = "../../res/big.txt";
+            string path = "../../res/random_data";
             
             LZ77 lz77 = new LZ77();
             DataFile compressed_file = lz77.Compress(new DataFile(path));
