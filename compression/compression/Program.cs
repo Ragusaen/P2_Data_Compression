4d388d6793a726c7307a821013c57c6635072bed
Ragusaen <rtollu18@student.aau.dk>
2019-03-13 11:25:44 +0100

    added print after compression

--- compression/compression/Program.cs
+++ compression/compression/Program.cs
@@ -8,12 +8,14 @@ namespace compression {
             var watch = System.Diagnostics.Stopwatch.StartNew();
 
             LZ77 lz77 = new LZ77();
-            DataFile compressed_file = lz77.Compress(new DataFile(path));
+            DataFile input_file = new DataFile(path);
+            DataFile compressed_file = lz77.Compress(input_file);
             
             compressed_file.WriteToFile("../../res/out");
             watch.Stop();
             var elapsedMs = watch.ElapsedMilliseconds;
             Console.WriteLine("\nTime elapsed" + elapsedMs / 1000 + "s");
+            Console.WriteLine("\nStørrelse før: " + input_file.Length + "\nStørrelse efter: " + compressed_file.Length + "\nKomprimeringsratio: " + (double)compressed_file.Length/input_file.Length);
         }
     }
 }
