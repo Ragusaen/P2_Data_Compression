1d5d6546e0074d9e67a1b85c26da59f554721464
Ragusaen <rtollu18@student.aau.dk>
2019-03-27 14:41:03 +0100

    Added BWT algorithm

--- compression/UnitTesting/Tests.cs
+++ compression/UnitTesting/Tests.cs
@@ -2,9 +2,88 @@
 using NUnit.Framework;
 
 using compression;
+using compression.NBWT;
 using compression.LZ;
 
 namespace UnitTesting{
+    [TestFixture, Category("BurrowsWheelerTransform")]
+    public class BWTTest {
+        [Test]
+        public void TransformReturnsBNNAAAFromBANANA() {
+            byte[] input = new byte[] {(byte)'^',(byte) 'B', (byte) 'A', (byte) 'N', (byte) 'A', (byte) 'N', (byte) 'A', (byte)'|'};
+            byte[] expected = new byte[] {(byte) 'B', (byte) 'N', (byte) 'N', (byte) '^', (byte) 'A', (byte) 'A', (byte) '|', (byte) 'A'};
+            
+            byte[] result = BWT.Transform(input);
+            
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void InverseTransformReturnsBANANAFromBNNAAA() {
+            byte[] expected = new byte[] {(byte)'^',(byte) 'B', (byte) 'A', (byte) 'N', (byte) 'A', (byte) 'N', (byte) 'A', (byte)'|'};
+            byte[] input = new byte[] {(byte) 'B', (byte) 'N', (byte) 'N', (byte) '^', (byte) 'A', (byte) 'A', (byte) '|', (byte) 'A'};
+            
+            byte[] result = BWT.InverseTransform(input);
+            
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TransformReturnsFromFemFladeFloedeboller() {
+            string input = "^fem flade flødeboller på et fladt flødebolle fad|";
+            string expected = "åemtter|llfeeaøøaaldddfl  ^    ffllooffebb eeddpll"; //Has not been validated
+            
+            byte[] inArr = BWT.StringToByteArray(input);
+            byte[] output = BWT.Transform(inArr);
+            string actual = BWT.ByteArrayToString(output);
+            
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test]
+        public void InverseTransformReturnsFromFemFladeFloedeboller() {
+            string expected = "^fem flade flødeboller på et fladt flødebolle fad|";
+            string input = "åemtter|llfeeaøøaaldddfl  ^    ffllooffebb eeddpll"; //Has not been validated
+            
+            byte[] inArr = BWT.StringToByteArray(input);
+            byte[] output = BWT.InverseTransform(inArr);
+            string actual = BWT.ByteArrayToString(output);
+            
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TransformFromText1() {
+            string input = "^This is some small file, that noone wants to read because it is boring. Therefore it has been hidden here. Please do not share with any hostile intelligence.|";
+            string expected = "..hdsseltnnyetseetootes,seeeege  ^|emw hhehc   enaid slrsmnrlrcrlbbrtdeghh enits t T T hltfr  h   wliiPlaesoeeeoi  iaadtsonfbhnoae eoaiiitau   oiiaoni s na  n."; //Has not been validated
+            
+            byte[] inArr = BWT.StringToByteArray(input);
+            byte[] output = BWT.Transform(inArr);
+            string actual = BWT.ByteArrayToString(output);
+            
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void InverseTransformFromText1() {
+            string expected = "^This is some small file, that noone wants to read because it is boring. Therefore it has been hidden here. Please do not share with any hostile intelligence.|";
+            string input = "..hdsseltnnyetseetootes,seeeege  ^|emw hhehc   enaid slrsmnrlrcrlbbrtdeghh enits t T T hltfr  h   wliiPlaesoeeeoi  iaadtsonfbhnoae eoaiiitau   oiiaoni s na  n."; //Has not been validated
+            
+            byte[] inArr = BWT.StringToByteArray(input);
+            byte[] output = BWT.InverseTransform(inArr);
+            string actual = BWT.ByteArrayToString(output);
+            
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ByteArrayToStringFromFile3() {
+            string path = TestContext.CurrentContext.TestDirectory + "../../../res/testfile1";
+            DataFile input_file = new DataFile(path);
+            
+        }
+    }
+    
     [TestFixture, Category("DataFile")]
     public class DataFileTest{
         [Test]
@@ -416,7 +495,6 @@ namespace UnitTesting{
             Assert.AreEqual(expected, actual);
         }
     }
-
 
     [TestFixture, Category("SlidingWindow")]
     public class TestSlidingWindow {
