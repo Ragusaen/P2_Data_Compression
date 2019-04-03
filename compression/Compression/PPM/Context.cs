98428daef4ccb2cffc7f6a073be75c15b1532f16
KristianOdum <kani18@student.aau.dk>
2019-04-03 17:13:36 +0200

    Added more test and modified PPM code

--- compression/Compression/PPM/Context.cs
+++ compression/Compression/PPM/Context.cs
@@ -4,15 +4,14 @@ namespace Compression.PPM{
     public class Context{
         public byte[] ContextBytes;
         public List<Symbol> SymbolList = new List<Symbol>();
-
-        public Context(byte[] contextBytes, byte firstLetter) {
+        
+        public Context(byte[] contextBytes) {
             ContextBytes = contextBytes;
-            SymbolList.Add(new Symbol(firstLetter));
         }
-
+        
         public void Update(byte a) {
             for(int i = 0; i < SymbolList.Count; i++) {
-                if (SymbolList[i].Letter == a) {
+                if ((SymbolList[i].Data as Letter).Data == a) {
                     SymbolList[i].Count++;
                     return;
                 }
--- compression/Compression/PPM/ContextTable.cs
+++ compression/Compression/PPM/ContextTable.cs
@@ -5,8 +5,8 @@ using System.Security.Cryptography;
 
 namespace Compression.PPM{
     public class ContextTable{
-        public List<Context> ContextList; 
-        public int TotalCount { get; set; }
+        public List<Context> ContextList = new List<Context>();
+        public uint TotalCount;
 
         public void UpdateContext(byte[] context, byte symbol) {
             int i = ContextAlreadyExist(context);
@@ -15,18 +15,30 @@ namespace Compression.PPM{
                 ContextList[i].Update(symbol);
             }
             else { // execute if context was not found in table
-                ContextList.Add(new Context(context, symbol));
+                ContextList.Add(new Context(context));
+                ContextList[0].Update(symbol);
             }
         }
 
         private int ContextAlreadyExist(byte[] currentContext) {
             ByteArrayComparer byteArrayComparer = new ByteArrayComparer();
             
+            // Looks for context in order table, if found returns the index value
             for (int i = 0; i < ContextList.Count; i++) {
                 if(byteArrayComparer.Compare(ContextList[i].ContextBytes, currentContext) == 0)
                     return i; 
             }
             return -1;
+        }
+
+        public void CalculateTotalCount() {
+            TotalCount = 0;
+            
+            for (int i = 0; i < ContextList.Count; i++) {
+                for (int j = 0; j < ContextList[i].SymbolList.Count; j++) {
+                    TotalCount += ContextList[i].SymbolList[j].Count;
+                }
+            }
         }
     }
 }
