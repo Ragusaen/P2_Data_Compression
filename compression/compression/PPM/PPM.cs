57cf8dc6e5f678716ef1e3033d1cc46459062b81
KristianOdum <kani18@student.aau.dk>
2019-04-03 13:23:59 +0200

    Added PPM files and wrote some code

--- compression/compression/PPM/PPM.cs
+++ compression/compression/PPM/PPM.cs
@@ -1,16 +1,4 @@
-namespace compression.PPM{
-    public class PPM : Compressor{
-        protected override DataFile InternalCompress(DataFile input) {
-            
-            
-            
-            
-            DataFile output = new DataFile();
-            return output;
-        }    
-        
-        public override DataFile Decompress(DataFile dataFile) {
-            throw new System.NotImplementedException();
-        }    
-    }
+$HEADER$namespace $NAMESPACE$
+{
+  public class $CLASS$ {$END$}
 }
