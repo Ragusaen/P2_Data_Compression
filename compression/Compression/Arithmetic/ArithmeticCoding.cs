81b6a727a38555d66214d8bee5a5f8cec4e4fa1f
AirealAble <44190630+AirealAble@users.noreply.github.com>
2019-04-09 10:11:50 +0200

    Starten på arithmetisk kodning

---
+++ compression/Compression/Arithmetic/ArithmeticCoding.cs
@@ -0,0 +1,14 @@
+using Compression.PPM;
+using System.Collections.Generic;
+
+namespace Compression.Arithmetic
+{
+    public class ArithmeticCoding
+    {
+        DataFile compress(DataFile input, List<ContextTable> ppmTables)
+        {
+            
+            return input;
+        }
+    }
+}
