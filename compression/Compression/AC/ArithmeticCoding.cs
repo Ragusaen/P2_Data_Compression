2cf1a80e4d4f393723dab732a1ff1ffba7a68d6f
KristianOdum <kani18@student.aau.dk>
2019-04-11 15:00:20 +0200

    fixed brackets in AC

--- compression/Compression/AC/ArithmeticCoding.cs
+++ compression/Compression/AC/ArithmeticCoding.cs
@@ -1,13 +1,9 @@
 using Compression.PPM;
 using System.Collections.Generic;
 
-namespace Compression.AC
-{
-    public class ArithmeticCoding
-    {
-        public DataFile Compress(DataFile input, List<ContextTable> ppmTables)
-        {
-            
+namespace Compression.AC{
+    public class ArithmeticCoding{
+        public DataFile Compress(DataFile input, List<ContextTable> ppmTables) {
             return input;
         }
     }
