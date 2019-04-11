5631449b5bddff2b7f85cd712edbf316b8d92892
Roni Horo <rhoro18@student.aau.dk>
2019-04-11 15:52:22 +0200

    Entropy and corresponding unittest

--- compression/UnitTesting/Entropy/EntropyTest.cs
+++ compression/UnitTesting/Entropy/EntropyTest.cs
@@ -3,24 +3,37 @@ using NUnit.Framework;
 using Compression;
 using Compression.Entropy;
 
-namespace UnitTesting.Entropy
-{
+namespace UnitTesting.Entropy {
     [TestFixture, Category("Entropy")]
-    public class EntropyTest
-    {
-        public class logCalcTest
-        {
+    public class EntropyTest {
+        public class LogCalcTest {
             [Test]
-            public void logarithmTest()
-            {
-                double a = 0;
-                double b = 2;
+            public void LogarithmTest() {
+                double b;
+                double a;
+                a = 1;
+                b = 2;
 
-                double expectedResult = 0;
-                double result = logCalc(a, b);
+                const double expectedResult = 0;
+                var cee = new Compression.Entropy.Entropy();
+                var result = cee.LogCalc(a, b);
 
-                Assert.AreEqual(expectedResult, result);
+                Assert.AreEqual(expectedResult, actual: result);
             }
+        }
+
+        public class EntropyCalcTest {
+            [Test]
+            public void StringFrequency() {
+                var cef = new Compression.Entropy.Entropy();
+                var results = cef.CalcEntropy("fem flade fldeboller p et fladt fldebollefad");
+
+                const double expectedResult = 3.2540779268269016; 
+                
+                Assert.AreEqual(expectedResult,results);
+
+            }
+            
         }
     }
 }
