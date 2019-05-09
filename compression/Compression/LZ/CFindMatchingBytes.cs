c238e6d47f22d53a35b63a8edf9ade1ee68bf140
Ragusaen <rtollu18@student.aau.dk>
2019-05-09 14:38:48 +0200

    Optimized LZ

--- compression/Compression/LZ/CFindMatchingBytes.cs
+++ compression/Compression/LZ/CFindMatchingBytes.cs
@@ -7,21 +7,7 @@ using Compression.ByteStructures;
 
 namespace Compression.LZ {
     public class CFindMatchingBytes {
-
-#if TEST
-        private const string path = "CWrappers/libfindmatchingbytes.so";
-#else
-        private const string path = "../../CWrappers/libfindmatchingbytes.so";
-#endif
-        
-        [DllImport(path, EntryPoint = "print")]
-        public static extern void print(string message);
-
-        public static MatchPointer FindLongestMatch(ArrayIndexer<byte> haystack, ArrayIndexer<byte> needle) {
-            return FindLongestMatchWrapper(haystack.Array, haystack.Index, haystack.Length, needle.Array, needle.Length);
-        }
-
-        [DllImport(path, EntryPoint = "find_longest_match")]
-        private static extern MatchPointer FindLongestMatchWrapper(byte[] haystack, int haystack_index, int haystack_length, byte[] needle, int needle_length );
+        [DllImport("findmatchingbytes", EntryPoint = "find_longest_match")]
+        public static extern MatchPointer FindLongestMatch(ArrayIndexer<byte> haystack, ArrayIndexer<byte> needle);
     }
 }
