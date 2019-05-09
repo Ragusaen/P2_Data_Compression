637d5e16bb526bdd651bffb90987b1534b30a4dd
Mark <mvanki18@student.aau.dk>
2019-05-09 14:45:38 +0200

    Huffman kodning

--- compression/Compression/Huffman/Encode.cs
+++ compression/Compression/Huffman/Encode.cs
@@ -1,9 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Compression;
+using Compression.ByteStructures;
 
 namespace Compression.Huffman {
     public class Encode {
+        public byte[] HuffmanEncode(byte[] data)
+        {
+            List<Nodes> ListOfNodes = HuffmanNodes(data);
+
+            var TreeOfNodes = new Tree();
+            Nodes NodeTree = TreeOfNodes.CreateTree(ListOfNodes);
+
+            List<UnevenByte> NodeList = TreeOfNodes.TreeMap(NodeTree);
+            Dictionary<byte, UnevenByte> NodeDict = TreeOfNodes.SetCode(NodeTree);
+
+            return EncodeEveryByteFromData(NodeList, NodeDict, data);
+        }
+
+
         public List<Nodes> HuffmanNodes(byte[] data) {
             List<Nodes> ListOfNodes = new List<Nodes>(); {
                 for (int i = 0; i < data.Length; i++) {
@@ -15,11 +29,40 @@ namespace Compression.Huffman {
                     }
                 ListOfNodes.Sort();
                 }
-               return ListOfNodes;
             }
-            //Tree TreeOfList = new Tree();
+            return ListOfNodes;
+        }
 
-            //Nodes HuffmanTree = TreeOfList.CreateTree(ListOfNodes);
+        public byte[] EncodeEveryByteFromData (List<UnevenByte> NodeList, Dictionary<byte, UnevenByte> NodeDict, byte[] data) {
+            for (int i = 0; i < data.Length; i++) {
+                NodeList.Add(NodeDict[data[i]]);
+            }
+
+            UnevenByte ub = CreateUnevenByteWithAll_1(NodeList);
+            NodeList.Insert(0, ub);
+
+            var unevenByteConverter = new UnevenByteConverter();
+
+            return unevenByteConverter.UnevenBytesToBytes(NodeList);
+        }
+
+        private UnevenByte CreateUnevenByteWithAll_1(List<UnevenByte> NodeList) {
+            UnevenByte ub = new UnevenByte();
+            int stabilizer = 0;
+
+            for (int i = 0; i < NodeList.Count; i++)
+            {
+                stabilizer += NodeList[i].Length;
+            }
+
+            stabilizer = (8 - (stabilizer % 8)) % 8;
+
+            for (int j = 0; j < stabilizer; j++)
+            {
+                ub += new UnevenByte(0b1, 1);
+            }
+
+            return ub;
         }
     }
 }
--- compression/UnitTesting/Huffman/TreeCode.cs
+++ compression/UnitTesting/Huffman/TreeCode.cs
@@ -6,173 +6,164 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using Compression;
 using Compression.Huffman;
+using Compression.ByteStructures;
 
 namespace UnitTesting.Huffman
 {
     [TestFixture, Category("HuffmanLeafList")]
-    class TreeCode
-    {
+    class TreeCode {
         [Test]
-        public void TreeMapOver_AB()
-        {
+        public void TreeMapOver_AB() {
             byte[] input = { (byte)'A', (byte)'B' };
-            string expected = "0101000001101000010"; //01A1B
+
+            byte[] expected = { 0b11101010, 0b00001101, 0b00001001 };
 
             Encode ListOf_AB = new Encode();
             List<Nodes> temp_list = ListOf_AB.HuffmanNodes(input);
             Tree TreeOf_AB = new Tree();
             Nodes temp_tree = TreeOf_AB.CreateTree(temp_list);
 
-            string actual = TreeOf_AB.TreeMap(temp_tree);
+            Dictionary<byte, UnevenByte> NodeDict = TreeOf_AB.SetCode(temp_tree);
+            List<UnevenByte> NodeList = TreeOf_AB.TreeMap(temp_tree);
+
+            byte[] actual = ListOf_AB.EncodeEveryByteFromData(NodeList, NodeDict, input);
 
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
-        public void TreeMapOver_AABB()
-        {
+        public void TreeMapOver_AABB() {
             byte[] input = { (byte)'A', (byte)'A', (byte)'B', (byte)'B' };
-            string expected = "0101000001101000010"; //01A1B
+
+            byte[] expected = { 0b10101000, 0b00110100, 0b00100011 };
 
             Encode ListOf_AABB = new Encode();
             List<Nodes> temp_list = ListOf_AABB.HuffmanNodes(input);
             Tree TreeOf_AABB = new Tree();
             Nodes temp_tree = TreeOf_AABB.CreateTree(temp_list);
 
-            string actual = TreeOf_AABB.TreeMap(temp_tree);
+            Dictionary<byte, UnevenByte> NodeDict = TreeOf_AABB.SetCode(temp_tree);
+            List<UnevenByte> NodeList = TreeOf_AABB.TreeMap(temp_tree);
+
+            byte[] actual = ListOf_AABB.EncodeEveryByteFromData(NodeList, NodeDict, input);
 
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
-        public void TreeMapOver_ABC()
-        {
+        public void TreeMapOver_ABC() {
             byte[] input = { (byte)'A', (byte)'B', (byte)'C' };
-            string expected = "01010000110101000001101000010"; //01C01A1B
+
+            byte[] expected = { 0b11111101, 0b01000011, 0b01010000, 0b01101000, 0b01010110 };
 
             Encode ListOf_ABC = new Encode();
             List<Nodes> temp_list = ListOf_ABC.HuffmanNodes(input);
             Tree TreeOf_ABC = new Tree();
             Nodes temp_tree = TreeOf_ABC.CreateTree(temp_list);
 
-            string actual = TreeOf_ABC.TreeMap(temp_tree);
+            Dictionary<byte, UnevenByte> NodeDict = TreeOf_ABC.SetCode(temp_tree);
+            List<UnevenByte> NodeList = TreeOf_ABC.TreeMap(temp_tree);
+
+            byte[] actual = ListOf_ABC.EncodeEveryByteFromData(NodeList, NodeDict, input);
 
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
-        public void TreeMapOver_ABCC()
-        {
+        public void TreeMapOver_ABCC() {
             byte[] input = { (byte)'A', (byte)'B', (byte)'C', (byte)'C' };
-            string expected = "00101000001101000010101000011"; //001A1B1C
+
+            byte[] expected = { 0b11111001, 0b01000001, 0b10100001, 0b01010000, 0b11000111 };
 
             Encode ListOf_ABCC = new Encode();
             List<Nodes> temp_list = ListOf_ABCC.HuffmanNodes(input);
             Tree TreeOf_ABCC = new Tree();
             Nodes temp_tree = TreeOf_ABCC.CreateTree(temp_list);
 
-            string actual = TreeOf_ABCC.TreeMap(temp_tree);
+            Dictionary<byte, UnevenByte> NodeDict = TreeOf_ABCC.SetCode(temp_tree);
+            List<UnevenByte> NodeList = TreeOf_ABCC.TreeMap(temp_tree);
+
+            byte[] actual = ListOf_ABCC.EncodeEveryByteFromData(NodeList, NodeDict, input);
 
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
-        public void TreeMapOver_AABC()
-        {
+        public void TreeMapOver_AABC() {
             byte[] input = { (byte)'A', (byte)'A', (byte)'B', (byte)'C' };
-            string expected = "01010000010101000010101000011"; //01A01B1C
+
+            byte[] expected = { 0b11111010, 0b10000010, 0b10100001, 0b01010000, 0b11001011 };
 
             Encode ListOf_AABC = new Encode();
             List<Nodes> temp_list = ListOf_AABC.HuffmanNodes(input);
             Tree TreeOf_AABC = new Tree();
             Nodes temp_tree = TreeOf_AABC.CreateTree(temp_list);
 
-            string actual = TreeOf_AABC.TreeMap(temp_tree);
+            Dictionary<byte, UnevenByte> NodeDict = TreeOf_AABC.SetCode(temp_tree);
+            List<UnevenByte> NodeList = TreeOf_AABC.TreeMap(temp_tree);
+
+            byte[] actual = ListOf_AABC.EncodeEveryByteFromData(NodeList, NodeDict, input);
 
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
-        public void TreeMapOver_ABCD()
-        {
+        public void TreeMapOver_ABCD() {
             byte[] input = { (byte)'A', (byte)'B', (byte)'C', (byte)'D' };
-            string expected = "001010000011010000100101000011101000100"; //001A1B01C1D
 
-            Encode ListOf_ABC = new Encode();
-            List<Nodes> temp_list = ListOf_ABC.HuffmanNodes(input);
-            Tree TreeOf_ABC = new Tree();
-            Nodes temp_tree = TreeOf_ABC.CreateTree(temp_list);
+            byte[] expected = { 0b10010100, 0b00011010, 0b00010010, 0b10000111, 0b01000100, 0b00011011 };
 
-            string actual = TreeOf_ABC.TreeMap(temp_tree);
+            Encode ListOf_ABCD = new Encode();
+            List<Nodes> temp_list = ListOf_ABCD.HuffmanNodes(input);
+            Tree TreeOf_ABCD = new Tree();
+            Nodes temp_tree = TreeOf_ABCD.CreateTree(temp_list);
+
+            Dictionary<byte, UnevenByte> NodeDict = TreeOf_ABCD.SetCode(temp_tree);
+            List<UnevenByte> NodeList = TreeOf_ABCD.TreeMap(temp_tree);
+
+            byte[] actual = ListOf_ABCD.EncodeEveryByteFromData(NodeList, NodeDict, input);
 
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
-        public void TreeMapOver_ABCDEFGH()
-        {
+        public void TreeMapOver_ABCDEFGH() {
             byte[] input = { (byte)'A', (byte)'B', (byte)'C', (byte)'D', (byte)'E', (byte)'F', (byte)'G', (byte)'H' };
-            string expected = "0001010000011010000100101000011101000100001010001011010001100101000111101001000"; //0001A1B01C1D001E1F01G1H
+
+            byte[] expected = { 0b10001010, 0b00001101, 0b00001001, 0b01000011, 0b10100010, 0b00010100, 0b01011010,
+                                0b00110010, 0b10001111, 0b01001000, 0b00000101, 0b00111001, 0b01110111};
 
             Encode ListOf_ABC = new Encode();
             List<Nodes> temp_list = ListOf_ABC.HuffmanNodes(input);
             Tree TreeOf_ABC = new Tree();
             Nodes temp_tree = TreeOf_ABC.CreateTree(temp_list);
 
-            string actual = TreeOf_ABC.TreeMap(temp_tree);
+            Dictionary<byte, UnevenByte> NodeDict = TreeOf_ABC.SetCode(temp_tree);
+            List<UnevenByte> NodeList = TreeOf_ABC.TreeMap(temp_tree);
+
+            byte[] actual = ListOf_ABC.EncodeEveryByteFromData(NodeList, NodeDict, input);
 
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
-        public void TreeMapOver_BAABCCDD()
-        {
-            byte[] input = { (byte)'B', (byte)'A', (byte)'A', (byte)'B', (byte)'C', (byte)'C', (byte)'D', (byte)'D' };
-            string expected = "001010000011010000100101000011101000100"; //001A1B01C1D
-
-            Encode ListOfBAABBCA = new Encode();
-            List<Nodes> temp_list = ListOfBAABBCA.HuffmanNodes(input);
-            Tree TreeOf_BAABBCA = new Tree();
-            Nodes temp_node = TreeOf_BAABBCA.CreateTree(temp_list);
-
-
-            string actual = TreeOf_BAABBCA.TreeMap(temp_node);
-
-            Assert.AreEqual(expected, actual);
-        }
-        [Test]
-        public void TreeMapOver_BAABBCA()
-        {
-            byte[] input = { (byte)'B', (byte)'A', (byte)'A', (byte)'B', (byte)'B', (byte)'C', (byte)'A' };
-            string expected = "01010000100101000011101000001"; //01B01C1A
-
-            Encode ListOfBAABBCA = new Encode();
-            List<Nodes> temp_list = ListOfBAABBCA.HuffmanNodes(input);
-            Tree TreeOf_BAABBCA = new Tree();
-            Nodes temp_node = TreeOf_BAABBCA.CreateTree(temp_list);
-
-            string actual = TreeOf_BAABBCA.TreeMap(temp_node);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [Test]
-        public void TreeMapOver_testfile2()
-        {
+        public void TreeMapOver_testfile2() {
             string path = "fem flade flødeboller på et fladt flødebollefad"; // brug testfile2 i stedet for
             byte[] input = ByteMethods.StringToByteArray(path);
 
-            string expected = "0000101101111010111001011110010101011101001111110000101100100100100000001011001100101100001010110001001011011011011100000101100101101101100";
-            //00001o01r1å01t1ø01d1 001f01a01b01m1p01e1l
+            int expected = 38;
 
             Encode ListOf_testfile2 = new Encode();
             List<Nodes> temp = ListOf_testfile2.HuffmanNodes(input);
             Tree TreeOf_testfile2 = new Tree();
-            Nodes temp_node = TreeOf_testfile2.CreateTree(temp);
+            Nodes temp_tree = TreeOf_testfile2.CreateTree(temp);
 
-            string actual = TreeOf_testfile2.TreeMap(temp_node);
+            Dictionary<byte, UnevenByte> NodeDict = TreeOf_testfile2.SetCode(temp_tree);
+            List<UnevenByte> NodeList = TreeOf_testfile2.TreeMap(temp_tree);
 
-            Assert.AreEqual(expected, actual);
+            byte[] actual = ListOf_testfile2.EncodeEveryByteFromData(NodeList, NodeDict, input);
+
+            Assert.AreEqual(expected, actual.Length);
         }
     }
 }
